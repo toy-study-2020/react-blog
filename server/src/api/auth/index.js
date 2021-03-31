4a50951968cf7a80c6b63caec0a98aa1e4e94ca3
baekCode <baekcode@gmail.com>
2021-03-31 23:35:00 +0900

    feat: auth API login POST Call Skeleton

--- server/src/api/auth/index.js
+++ server/src/api/auth/index.js
@@ -8,9 +8,9 @@ const auth = new Router();
  * POST : register {Id,Password,PasswordConform,Name,Email,Gender}
  * */
 
-auth.get('/', ctx => {
-  ctx.status = 200;
-  ctx.body = 'GET : auth success!!'
+auth.post('/login', ctx => {
+  const {id, password} = ctx.request.body
+  ctx.body = {id,password}
 })
 
 export default auth;
