656ebf3a27ce0dcd8a81ab7555713751f9f2d57a
baekcode <baekcode@gmail.com>
2021-04-02 19:08:36 -0700

    feat: connect mongoDB

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,5 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  PORT: 4000,
+  PORT     : process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,23 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
 import config from './config/index.js';
 import api from './api/index.js';
 
 const app = new Koa();
 const router = new Router();
-const {PORT} = config;
+const {PORT, MONGO_URI} = config;
+
+mongoose
+  .connect(MONGO_URI, {useNewUrlParser: true, useFindAndModify: false})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.error(e);
+  });
+
 
 router.use('/api', api.routes());
 app.use(bodyParser());
