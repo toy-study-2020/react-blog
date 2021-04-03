320bc490c18fbfbd1f3d22fb1b7eecf8a91393a6
baekcode <baekcode@gmail.com>
2021-04-03 15:41:47 +0900

    feat: posts API skeleton

--- server/src/api/posts/index.js
+++ server/src/api/posts/index.js
@@ -2,8 +2,33 @@ import Router from 'koa-router';
 
 const posts = new Router();
 
+/**
+ * post API
+ * [GET] 게시글 전체 조회 : /api/posts/
+ * [POST] 게시글 작성 : /api/posts/ : {title, body, user, date}
+ * [GET] 특정 게시글 조회 : /api/posts/:id
+ * [PATCH] 특정 게시글 수정 : /api/posts/:id
+ * [DELETE] 특정 게시글 삭 : /api/posts/:id제
+ * */
+
 posts.get('/', ctx => {
   ctx.body = 'GET : posts success!';
+});
+
+posts.post('/', ctx => {
+
+});
+
+posts.get('/:id', ctx => {
+
+});
+
+posts.patch('/:id', ctx => {
+
+});
+
+posts.delete('/:id', ctx => {
+
 });
 
 export default posts;
