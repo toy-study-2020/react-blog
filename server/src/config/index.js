1562af3df03a50c3be651423bb937e196c84a643
baekcode <baekcode@gmail.com>
2021-04-02 19:58:52 -0700

    feat: auth Schema module method

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  PORT     : process.env.PORT,
-  MONGO_URI: process.env.MONGO_URI
+  PORT      : process.env.PORT,
+  MONGO_URI : process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET
 };
