923519f11d12719d2199b7adbe7931b7fd18356b
baekcode <baekcode@gmail.com>
2021-04-02 18:42:44 -0700

    chore: 오타수정

--- server/src/modules/auth.js
+++ server/src/modules/auth.js
@@ -4,14 +4,14 @@ import bcrypt from 'bcrypt';
 const {Scheme} = mongoose;
 
 //id, password, name, email
-const UserScheme = new Scheme({
+const UserSchema = new Scheme({
   id            : String,
   hashedPassword: String,
   name          : String,
   email         : String
 });
 
-UserScheme.method.setPassword = async (password) => {
+UserSchema.method.setPassword = async (password) => {
   const hash = await bcrypt.hash(password, 10);
   this.hashedPassword = hash;
 };
