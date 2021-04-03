aa32d5e434d40b8971644bd2489ffe5f01c11286
baekcode <baekcode@gmail.com>
2021-04-02 21:19:22 -0700

    feat: auth API login

--- server/src/api/auth/index.js
+++ server/src/api/auth/index.js
@@ -11,8 +11,38 @@ const auth = new Router();
  * */
 
 auth.post('/login', async ctx => {
-  const {id, password} = ctx.request.body;
-  ctx.body = {id, password};
+  const {userId, password} = ctx.request.body;
+
+  if (!userId || !password) {
+    ctx.status = 401;
+    return;
+  }
+
+  try {
+    const user = await User.findByUserId(userId);
+
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+
+    const valid = await user.checkPassword(password);
+
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+
+    ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge  : 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true
+    });
+
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 });
 
 auth.post('/register', async ctx => {
