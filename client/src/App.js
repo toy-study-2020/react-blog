39ac5500a228aec3af75b8b1cf93d1ff9044d337
mjkang0987 <mjkang0987@interpark.com>
2021-04-06 16:52:22 +0900

    chore: App.js 내 사용하지 않는 useState import 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import Member from './containers/Member';
-import { useState } from 'react';
+import Member from './containers/member/Member';
 
 function App() {
   return (
