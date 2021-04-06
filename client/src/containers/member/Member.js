dd6352a10319415f998ecc135d9d0ae305f9f0f8
mjkang0987 <mjkang0987@interpark.com>
2021-04-06 17:27:53 +0900

    feat: 로그인/회원가입 Member.js 컨테이너 추가

---
+++ client/src/containers/member/Member.js
@@ -0,0 +1,17 @@
+import FormGroup from '../../components/FormGroup';
+import { REGISTER } from '../../libs/constants';
+
+const Member = ({match, location, history}) => {
+  // 로그인 케이스 임시처리
+  const isLogin = match.path.indexOf('login') > -1;
+  const formsKey = Object.keys(REGISTER);
+  const forms = isLogin ? formsKey.slice(0, 2) : formsKey;
+  return (
+    <article>
+      <h2>{isLogin ? '로그인' : '회원가입'}</h2>
+      <FormGroup forms={forms} />
+    </article>
+  );
+};
+
+export default Member;
