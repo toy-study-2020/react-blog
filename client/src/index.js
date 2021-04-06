d274cb59777d046e1de3a397f73ca4a94b30a382
baekCode <baekcode@gmail.com>
2021-04-06 23:29:56 +0900

    feat: redux 기본세팅(reduxSaga)

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer, {rootSaga} from './modules';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
