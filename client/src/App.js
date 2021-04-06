641ba50eb7682f3df1f3079a3a809e9215f6175e
mjkang0987 <mjkang0987@interpark.com>
2021-04-06 16:22:45 +0900

    feat: 로그인/회원가입 Router 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Member from './containers/Member';
+import { useState } from 'react';
+
 function App() {
   return (
-    <>Hi</>
+    <Router>
+      <Route
+        path="/member/register"
+        render={Member}/>
+      <Route
+        path="/member/login"
+        render={Member}/>
+      {/*<Redirect path="*" to="/" />*/}
+    </Router>
   );
 }
 
