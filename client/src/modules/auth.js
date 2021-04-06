6d3747b2bed3c05d975b85a033a0dd2dd88e7441
baekCode <baekcode@gmail.com>
2021-04-07 00:01:31 +0900

    feat: auth module todos...(액션함수, 리듀서)

--- client/src/modules/auth.js
+++ client/src/modules/auth.js
@@ -4,6 +4,12 @@ import {createActionTypes} from '../lib/reduxUtils';
 const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createActionTypes('auth/REGISTER');
 const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createActionTypes('auth/LOGIN');
 
+/**
+ * @todo auth actions 정의 [2021-04-07]
+ * */
+export const register = createAction(REGISTER, () => ({}));
+export const login = createAction(LOGIN, () => ({}));
+
 const initialState = {
   register: {
     userId         : '',
@@ -19,6 +25,9 @@ const initialState = {
   error   : null
 };
 
+/**
+ * @todo auth handle 정의 [2021-04-07]
+ * */
 const auth = handleActions({
   [REGISTER]        : state => ({}),
   [REGISTER_SUCCESS]: state => ({}),
