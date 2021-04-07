ae2f75884fb3388e2255f2b947b9e720376de5cc
baekCode <44539224+baekCode@users.noreply.github.com>
2021-04-08 00:05:00 +0900

    feat: redux test

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,16 @@ import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer, {rootSaga} from './modules';
 import App from './App';
+import {loginAction} from './modules/auth';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
+
+const reduxTest = () => {
+  store.dispatch(loginAction({userId:'test', password:'1234'}))
+}
+reduxTest()
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
