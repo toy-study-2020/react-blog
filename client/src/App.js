027cd4695d401946a25f4e8485cf187fa29b2a51
mjkang0987 <mjkang0987@interpark.com>
2021-04-07 12:31:38 +0900

    feat: Header, Footer 컴포넌트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Member from './containers/member/Member';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Route
         path="/member/register"
         render={Member}/>
       <Route
         path="/member/login"
         render={Member}/>
-      <Redirect path="*" to="/" />
+      {/*<Redirect path="*" to="/" />*/}
+      <Footer />
     </Router>
   );
 }
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = _ => {
+  return (
+    <header>
+      <button type="button">
+        <span>메뉴</span>
+      </button>
+    </header>
+  );
+};
+
+export default Header;
