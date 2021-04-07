43bac995838f459d2eeec1063a0602aebfa82f8d
baekCode <44539224+baekCode@users.noreply.github.com>
2021-04-08 00:01:18 +0900

    feat: auth action handler (login, register)

--- client/src/modules/auth.js
+++ client/src/modules/auth.js
@@ -5,8 +5,13 @@ import {API, createActionTypes, createRequestSaga} from '../lib/reduxUtils';
 const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createActionTypes('auth/REGISTER');
 const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createActionTypes('auth/LOGIN');
 
-export const register = createAction(REGISTER, ({userId, password, name, email}) => ({userId, password, name, email}));
-export const login = createAction(LOGIN, ({userId, password}) => ({userId, password}));
+export const registerAction = createAction(REGISTER, ({userId, password, name, email}) => ({
+  userId,
+  password,
+  name,
+  email
+}));
+export const loginAction = createAction(LOGIN, ({userId, password}) => ({userId, password}));
 
 const registerSaga = createRequestSaga(REGISTER, API.auth.register);
 const loginSaga = createRequestSaga(LOGIN, API.auth.login);
@@ -28,16 +33,28 @@ const initialState = {
     userId  : '',
     password: ''
   },
+  user    : null,
   error   : null
 };
 
-/**
- * @todo auth handle 정의 [2021-04-07]
- * */
 const auth = handleActions({
-  [REGISTER_SUCCESS]: state => ({}),
+  [REGISTER_SUCCESS]: (state, {payload: user}) => {
+    console.log('[register handleActions]', user);
+    return {
+      ...state,
+      error: null,
+      user
+    };
+  },
   [REGISTER_FAILURE]: state => ({}),
-  [LOGIN_SUCCESS]   : state => ({}),
+  [LOGIN_SUCCESS]   : (state, {payload: user}) => {
+    console.log('[login handleActions]', user);
+    return {
+      ...state,
+      error: null,
+      user
+    };
+  },
   [LOGIN_FAILURE]   : state => ({})
 }, initialState);
 
