67492c4962743a45cbe340614ad3a3fd2775434e
baekCode <44539224+baekCode@users.noreply.github.com>
2021-04-07 22:26:09 +0900

    feat: lib, createRequestSaga

--- client/src/lib/reduxUtils.js
+++ client/src/lib/reduxUtils.js
@@ -1,1 +1,23 @@
+import {call, put} from 'redux-saga/effects';
+
 export const createActionTypes = type => ([type, `${type}_SUCCESS`, `${type}_FAILURE`]);
+
+export const createRequestSaga = (type, apiCall) => {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return function* (action) {
+    try {
+      const response = yield call(apiCall, action.payload);
+      put({
+        type   : SUCCESS,
+        payload: response.data
+      });
+    } catch (e) {
+      put({
+        type   : FAILURE,
+        payload: e
+      });
+    }
+  };
+};
