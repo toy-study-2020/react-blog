2f7b2c8b54d07bec69a00bb923a116998f44e251
Minjeong kang <snowdrop1202@gmail.com>
2021-04-08 23:25:05 +0900

    feat: reset css 추가 #17

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,62 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Member from './containers/member/Member';
 import Main from './containers/main/Main';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  body,
+  section,
+  article,
+  main,
+  header,
+  footer,
+  div,
+  ul,
+  li,
+  button,
+  a {
+    margin: 0;
+    padding: 0;
+  }
+  
+  ul,
+  li {
+    list-style: none;
+  }
+  
+  button,
+  a {
+    appearance: none;
+    border: none;
+    background: transparent;
+    
+    &:active {
+      opacity: .6;
+    }
+  }
+`
 function App() {
   return (
-    <Router>
-      <Header />
-      <main>
-        <Route
-          exact
-          path="/"
-          render={Main}/>
-        <Route
-          path="/member/register"
-          render={Member}/>
-        <Route
-          path="/member/login"
-          render={Member}/>
-        <Redirect path="*" to="/" />
-      </main>
-      <Footer />
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Header />
+        <main>
+          <Route
+            exact
+            path="/"
+            render={Main}/>
+          <Route
+            path="/member/register"
+            render={Member}/>
+          <Route
+            path="/member/login"
+            render={Member}/>
+          <Redirect path="*" to="/" />
+        </main>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
