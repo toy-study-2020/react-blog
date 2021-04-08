856a184aaaa311dc523aa6af35e1e40ade86cb6a
Minjeong kang <snowdrop1202@gmail.com>
2021-04-09 00:00:05 +0900

    feat: Header 컴포넌트 styled-component 추가
    * 메뉴 여/닫힘 상태 useState 추가

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,48 @@
 import { Link } from 'react-router-dom';
 import { ROUTER } from '../libs/constants';
+import styled from 'styled-components';
+import { useState } from 'react';
 
+const HeaderStyle = styled.header `
+  display: block;
+  position: relative;
+  
+  button {
+    position: relative;
+    width: 50px;
+    height: 50px;
+    background-color: #f0f0f0;
+    text-indent: -9999px;
+    
+    &[aria-expanded="true"] {
+      + nav {
+        display: flex;;
+      }
+    }
+    
+    span:before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+  }
+  
+  nav {
+    display: none;
+  }
+`;
 const Header = _ => {
+  const [nav, setNav] = useState(false);
+  const handlerNav = _ => {
+    setNav(!nav);
+  };
   return (
-    <header>
+    <HeaderStyle>
       <button
         type="button"
-        aria-expanded="false">
+        aria-expanded={nav}
+        onClick={handlerNav}>
         <span>메뉴</span>
       </button>
       <nav>
@@ -15,7 +51,7 @@ const Header = _ => {
           {ROUTER.member.map(r => <li key={r}><Link to={`/member/${r}`}>{r.toUpperCase()}</Link></li>)}
         </ul>
       </nav>
-    </header>
+    </HeaderStyle>
   );
 };
 
