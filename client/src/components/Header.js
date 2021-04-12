c53154ba8f76c3f595a6012dcc9d8767564ef03e
mjkang0987 <mjkang0987@interpark.com>
2021-04-12 12:35:33 +0900

    chore: 네비게이션 버튼 아이콘, 트랜지션 추가

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,19 +12,53 @@ const HeaderStyle = styled.header `
     width: 50px;
     height: 50px;
     background-color: #f0f0f0;
+    padding: 10px;
+    box-sizing: border-box;
     text-indent: -9999px;
     
     &[aria-expanded="true"] {
+      span:before {
+        transform: rotate(45deg);
+        top: 24px
+      }
+      
+      span:after {
+        transform: rotate(-45deg);
+        bottom: 24px;
+      }
+      
       + nav {
-        display: flex;;
+        display: flex;
       }
     }
     
-    span:before {
-      content: "";
+    span {
+      display: block;
       position: absolute;
+      left: 0;
+      top: 0;
       width: 100%;
       height: 100%;
+    }
+    
+    span:before,
+    span:after {
+      content: "";
+      position: absolute;
+      left: 50%;
+      width: 30px;
+      height: 2px;
+      margin-left: -15px;
+      background-color: #000;
+      transition: transform .3s;
+    }
+    
+    span:before {
+      top: 14px;
+    }
+    
+    span:after {
+      bottom: 14px;
     }
   }
   
