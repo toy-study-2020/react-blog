b9c7d43c3e2f62655f2807b83f2b336e9e0755f8
Minjeong kang <snowdrop1202@gmail.com>
2021-04-17 01:10:51 +0900

    feat: main, footer 태그 스타일 추가 #22

--- client/src/styles/globalStyle.js
+++ client/src/styles/globalStyle.js
@@ -3,10 +3,31 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
   button,
   a {
     border: none;
     appearance: none;
+  }
+
+  main {
+    padding: 80px 100px 0;
+    min-height: calc(100% - 80px);
+    box-sizing: border-box;
+  }
+
+  footer {
+    display: flex;
+    height: 80px;
+    background-color: #1d1d1d;
+    color: #fff;
+    align-items: center;
+    justify-content: center;
   }
 `;
 
