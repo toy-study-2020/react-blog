ebc33c21967adb53fd1f1bbd3c8ddb7d3686b933
Minjeong kang <snowdrop1202@gmail.com>
2021-04-16 23:21:00 +0900

    feat: 회원가입 추가 #22
    * 로그인/회원가입 함수 분리하지않고 하나로 사용하기 위해서 onLogin 함수 이름 onClick으로 원복
    * 입력 인풋 검증 함수 추가

--- client/src/components/FormGroup.js
+++ client/src/components/FormGroup.js
@@ -1,16 +1,28 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { loginAction } from '../modules/auth';
+import { loginAction, registerAction } from '../modules/auth';
 
 const FormGroup = ({isLogin}) => {
   const dispatch = useDispatch();
   const {user} = useSelector(state => state.auth);
   const [input, setInput] = useState({
-    userId  : '',
-    password: ''
+    userId         : '',
+    password       : '',
+    passwordConfirm: '',
+    name           : '',
+    email          : '',
   });
 
-  const {userId, password} = input;
+  const {userId, password, name, email} = input;
+
+  const msg = {
+    userId         : 'ID를 입력하세요',
+    password       : '비밀번호를 입력하세요',
+    passwordConfirm: '비밀번호 확인을 입력하세요',
+    name           : '이름을 입력하세요',
+    email          : '이메일을 입력하세요'
+  };
+
   const onChange = e => {
     const target = e.target;
     const key = target.name;
@@ -20,12 +32,39 @@ const FormGroup = ({isLogin}) => {
       [key]: value
     });
   };
-  const onLogin = e => {
-    e.preventDefault();
-    if (!userId || !password) {
-      return alert('아이디, 비밀번호를 채워주세요');
+
+  const dispatchFunc = _ => {
+    if (isLogin) {
+      dispatch(loginAction({userId, password}));
+    } else {
+      dispatch(registerAction({userId, password, name, email}));
     }
-    dispatch(loginAction({userId, password}));
+  };
+
+  const checkNullish = ({target, arr}) => {
+    const nullishInput = arr.find(val => input[val] === '');
+
+    if (!isLogin && input['password'] !== input['passwordConfirm']) {
+      alert('비밀번호와 비밀번호 확인이 다릅니다.');
+      return target.passwordConfirm.focus();
+    }
+
+    if (nullishInput) {
+      alert(msg[nullishInput]);
+      return target[nullishInput].focus();
+    }
+
+    dispatchFunc();
+  };
+
+  const onClick = e => {
+    e.preventDefault();
+    const inputArr = Object.keys(input);
+    const validationInput = isLogin ? inputArr.slice(0, 2) : inputArr;
+    checkNullish({
+      arr   : validationInput,
+      target: e.target.closest('form')
+    });
   };
 
   useEffect(_ => {
@@ -91,7 +130,7 @@ const FormGroup = ({isLogin}) => {
       <button
         type="submit"
         className={isLogin ? 'btnLogin' : 'btnRegister'}
-        onClick={onLogin}>
+        onClick={onClick}>
         <span>
           {isLogin ? 'LOGIN' : 'REGISTER'}
         </span>
